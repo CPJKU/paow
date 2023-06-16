d59a611bdf53b2349bc87a5672ee99bb2f63c75a
sildater <silvandavidpeter@gmail.com>
2023-06-16 18:31:23 +0200

    fix bug in minimal midi test

--- paow/minimal_midi/test_minimal.py
+++ paow/minimal_midi/test_minimal.py
@@ -1,4 +1,5 @@
 import mido
+import time
 
 print(mido.get_output_names())
 
@@ -7,6 +8,10 @@ me = mido.Message("note_off", note = 64, velocity = 0)
 
 output_name = mido.get_output_names()[-1]
 o = mido.open_output(output_name)
-    
-o.send(ms, 0)
-o.send(me, 500)
+
+
+for k in range(19):
+    o.send(ms)
+    time.sleep(0.5)
+    o.send(me)
+    time.sleep(0.1)
