833f352a99f7689b3b7f316a753734880a3f9934
melkisedeath <manoskaristineos@gmail.com>
2023-06-20 17:46:20 +0200

    Material Changes for Grammar generation

--- paow/05_07_23/run.py
+++ paow/05_07_23/run.py
@@ -7,22 +7,24 @@ import os
 import tempfile
 import audiofile
 from audiomentations import Compose, AddGaussianNoise, TimeStretch, PitchShift, Shift
-import urllib
 import zipfile
 
 
 
 grammar = {
-    "Pattern" : [["P1", "A1", "D1"], ["P2", "A2", "D2"], ["P3", "A3", "D3"]],
-    "P1": np.array([67, 57, 53, 70]),
-    "P2": np.array([67, 57, 53, 70]),
-    "P3": np.array([67, 57, 53, 70]),
-    "A1": np.array([67, 57, 53, 70]),
-    "A2": np.array([67, 57, 53, 70]),
-    "A3": np.array([67, 57, 53, 70]),
-    "D1": np.array([300, 200, 500, 700]),
-    "D2": np.array([300, 200, 500, 700]),
-    "D3": np.array([300, 200, 500, 700]),
+    "Pattern" : [["P1", "A1", "D1", "W1"], ["P2", "A2", "D2", "W2"], ["P3", "A3", "D3", "W3"]],
+    "P1": np.array([67, 75, 71, 78, 77, 88]),
+    "P2": np.array([27, 98, 62, 55, 71, 63]),
+    "P3": np.array([55, 62, 69, 76, 81, 82, 88]),
+    "A1": np.array([30, 30, 30, 30, 30, 30]),
+    "A2": np.array([20, 20, 20, 20, 20, 20]),
+    "A3": np.array([20, 20, 20, 20, 20, 20, 20]),
+    "D1": np.array([300, 300, 300, 300, 300, 300]),
+    "D2": np.array([4000, 4000, 3000, 3000, 3000, 3000]),
+    "D3": np.array([3000, 3000, 3000, 3000, 3000, 3000, 3000]),
+    "W1": np.array([800, 800, 800, 800, 800, 800]),
+    "W2": np.array([0, 5000, 0, 0, 0, 3000]),
+    "W3": np.array([100, 100, 100, 100, 100, 100, 3000])
 }
 
 def audio_grammar(main_path):
@@ -42,7 +44,7 @@ def audio_grammar(main_path):
 
 
 
-def perturbation(p, a, d):
+def perturbation(p, a, d, w):
     new_p = np.array(p) + np.random.randint(-1, 2, len(p))
     new_a = np.array(a) + np.random.randint(-10, 10, len(a))
     add_mask = np.zeros(len(a))
@@ -50,9 +52,11 @@ def perturbation(p, a, d):
     add_mask[ind_a] = 10
     new_a = new_a + add_mask
     new_d = np.array(d) + np.random.randint(-1000, 1000, len(d))
+    new_w = np.array(w) + np.random.randint(-300, 300, len(d))
     # Filter out negative values
     new_d[new_d < 0] = 0
-    return new_p, new_a, new_d
+    new_w[new_w < 0] = 0
+    return new_p, new_a, new_d, new_w
 
 
 def audio_param_perturbation(parameters):
@@ -71,17 +75,17 @@ def generate_grammar(grammar, memory, mem_length=10):
         weights = np.arange(len(memory) + len(grammar["Pattern"]))
         pattern = random.choices(memory+grammar["Pattern"], weights=weights)[0]
 
-    p, a, d = pattern
+    p, a, d, w = pattern
     # Replace Letters
     ########
-    p, a, d = grammar[p], grammar[a], grammar[d]
+    p, a, d, w = grammar[p], grammar[a], grammar[d], grammar[w]
 
     # add to memory
     memory.append(pattern)
     # last element remove from memory if it exceeds size
     if len(memory) > mem_length:
         memory.pop(-1)
-    return perturbation(p, a, d), memory
+    return perturbation(p, a, d, w), memory
 
 
 def generate_audio_grammar(grammar, memory, effects, mem_length=10):
@@ -117,31 +121,38 @@ def generate_and_send_midi(music_grammar, port_name, generation_length=3600, mem
     """
     # Initialize port
     port = mido.open_output(port_name) if not test else None
-    tracked_generation_time = 0
+    # Hold down the pedal
+    msg = mido.Message('control_change', control=64, value=127)
+    port.send(msg)
+    start_time = time.time()
     memory = list()
-    while tracked_generation_time < generation_length*1000:
-        (p, a, d), memory = generate_grammar(music_grammar, memory, mem_length)
+    while time.time() - start_time < generation_length:
+        (p, a, d, w), memory = generate_grammar(music_grammar, memory, mem_length)
         # Generate midi message
         ########
-        tracked_generation_time += np.sum(d)
         # Send midi message
         for i in range(len(p)):
             if test:
                 print("Note: {}, Velocity: {}, Duration: {}".format(p[i], a[i], d[i]))
                 continue
-            msg = mido.Message('note_on', note=int(p[i]), velocity=int(a[i]), time=int(d[i]))
+            msg = mido.Message('note_on', note=int(p[i]), velocity=int(a[i]), time=0)
             port.send(msg)
-            time.sleep(d[i])
+            print(msg)
             msg = mido.Message('note_off', note=int(p[i]), velocity=int(a[i]), time=int(d[i]))
             port.send(msg)
-            time.sleep(d[i])
 
-            # Wait for the next generation
-            ########
-            time.sleep(0.1)
-            tracked_generation_time += 0.1*1000
+            # Wait for the next note
+            time.sleep(w[i]/1000)
+        # Wait for the next generation
+        ########
+        time.sleep(2)
+
+
     # Close port
     if not test:
+        # Release the pedal
+        msg = mido.Message('control_change', control=64, value=0)
+        port.send(msg)
         port.close()
 
 
@@ -184,14 +195,15 @@ class EffectClass:
 
 
 class GrammarGeneration:
-    def __init__(self, output_midi_port="dummy", generation_length=60, mem_length=10, ):
+    def __init__(self, output_midi_port="iM/ONE 1", generation_length=60, mem_length=10, ):
+        # Download audio files
         save_path = self.download_files()
         # Initialize Audio
         self.mixer = AudioMixing(total_duration=generation_length)
         # Initialize Audio grammar
         self.audio_grammar = audio_grammar(save_path)
         # Generate and send midi
-        generate_and_send_midi(grammar, port_name="dummy", generation_length=60, mem_length=10, test=True)
+        generate_and_send_midi(grammar, port_name=output_midi_port, generation_length=3600, mem_length=10, test=False)
 
     def download_files(self):
         import urllib
