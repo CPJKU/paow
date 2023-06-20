5cf2febb3da58d8874c36cf03e87407a369ec472
melkisedeath <manoskaristineos@gmail.com>
2023-06-20 16:17:57 +0200

    Some development on the generation algorithm.

--- paow/05_07_23/run.py
+++ paow/05_07_23/run.py
@@ -2,6 +2,13 @@ import numpy as np
 import mido
 import random
 import time
+import swmixer
+import os
+import tempfile
+import audiofile
+from audiomentations import Compose, AddGaussianNoise, TimeStretch, PitchShift, Shift
+import urllib
+import zipfile
 
 
 
@@ -18,6 +25,23 @@ grammar = {
     "D3": np.array([300, 200, 500, 700]),
 }
 
+def audio_grammar(main_path):
+    path1 = os.path.join(main_path, "X")
+    path2 = os.path.join(main_path, "Y")
+    path3 = os.path.join(main_path, "Z")
+    one = os.listdir(path1)
+    two = os.listdir(path2)
+    three = os.listdir(path3)
+    grammar = {
+        "Type": ["T1", "T2", "T3"],
+        "T1": one,
+        "T2": two,
+        "T3": three
+    }
+    return grammar
+
+
+
 def perturbation(p, a, d):
     new_p = np.array(p) + np.random.randint(-1, 2, len(p))
     new_a = np.array(a) + np.random.randint(-10, 10, len(a))
@@ -29,6 +53,12 @@ def perturbation(p, a, d):
     # Filter out negative values
     new_d[new_d < 0] = 0
     return new_p, new_a, new_d
+
+
+def audio_param_perturbation(parameters):
+    new_parameters = parameters
+    return parameters
+
 
 def generate_grammar(grammar, memory, mem_length=10):
     """Generate a pattern from the grammar"""
@@ -52,6 +82,24 @@ def generate_grammar(grammar, memory, mem_length=10):
     if len(memory) > mem_length:
         memory.pop(-1)
     return perturbation(p, a, d), memory
+
+
+def generate_audio_grammar(grammar, memory, effects, mem_length=10):
+    """Generate a pattern from the grammar"""
+    if len(memory) == 0:
+        # first pattern
+        pattern = random.choice(grammar["Type"])
+    else:
+        # weight more recent patterns
+        weights = np.arange(len(memory) + len(grammar["Type"]))
+        pattern = random.choices(memory+grammar["Type"], weights=weights)[0]
+
+    audio_clip, parameters = random.choice(grammar[pattern])
+
+    parameters = audio_param_perturbation(parameters)
+    path = effects.apply_effect(audio_clip, parameters)
+    return path
+
 
 def generate_and_send_midi(music_grammar, port_name, generation_length=3600, mem_length=10, test=False):
     """Generate a midi message and send it to the port
@@ -101,23 +149,69 @@ class AudioMixing:
     """
     Receives audio clips real time and plays them in a single stereo out.
     """
-    def __init__(self):
-        pass
+    def __init__(self, total_duration=3600, samplerate=44100, chunksize=1024, stereo=True):
+        swmixer.init(samplerate=samplerate, chunksize=chunksize, stereo=stereo)
+        swmixer.start()
+        self.total_duration = total_duration
 
-    def add_clip(self, audio_clip):
-        pass
+    def add_clip(self, audio_path, volume=0.5, time_until_next=60):
+        if time_until_next - self.total_duration > 0:
+            s = swmixer.StreamingSound(audio_path)
+            s.play(volume=volume)
+            self.total_duration -= time_until_next
 
-    def mix(self):
-        pass
+
+class EffectClass:
+
+    def __init__(self, sample_rate=44100):
+        self.sample_rate = sample_rate
+
+    def apply_effect(self, audio_path, params):
+        signal, sampling_rate = audiofile.read(audio_path)
+        signal = self.select_effect(params)
+        out = self.effect(signal, sampling_rate)
+        fp = tempfile.TemporaryFile()
+        audiofile.write(fp, out, sampling_rate)
+        return fp
+
+    def select_effect(self, params):
+        self.effect = Compose([
+            AddGaussianNoise(min_amplitude=params["min_amplitude"], max_amplitude=params["max_amplitude"], p=params["p"]),
+            TimeStretch(min_rate=params["min_rate"], max_rate=params["max_rate"], p=params["p"]),
+            PitchShift(min_semitones=params["min_semitones"], max_semitones=params["max_semitones"], p=params["p"]),
+            Shift(min_fraction=params["min_fraction"], max_fraction=params["max_fraction"], p=params["p"])
+        ])
 
 
+class GrammarGeneration:
+    def __init__(self, output_midi_port="dummy", generation_length=60, mem_length=10, ):
+        save_path = self.download_files()
+        # Initialize Audio
+        self.mixer = AudioMixing(total_duration=generation_length)
+        # Initialize Audio grammar
+        self.audio_grammar = audio_grammar(save_path)
+        # Generate and send midi
+        generate_and_send_midi(grammar, port_name="dummy", generation_length=60, mem_length=10, test=True)
+
+    def download_files(self):
+        import urllib
+        url = ""
+        testfile = urllib.URLopener()
+        save_path = os.path.join(os.path.dirname(__file__), "audio_files.zip")
+        testfile.retrieve(url, save_path)
+        # unzip files
+        with zipfile.ZipFile(save_path, 'r') as zip_ref:
+            zip_ref.extractall(os.path.dirname(__file__))
+        return os.path.dirname(__file__)
 
 
 
 if __name__ == "__main__":
-    # Generate and send midi
-    generate_and_send_midi(grammar, port_name="dummy", generation_length=60, mem_length=10, test=True)
-    # Close port
+    # Download Audio clips.
+    GrammarGeneration()
+
+
+
 
 
 
