ab8dae545266aced40cc16abeed12048a8ec146e
melkisedeath <manoskaristineos@gmail.com>
2023-07-25 18:00:27 +0200

    Added new grammar rules.

--- paow/23_07_26/run.py
+++ paow/23_07_26/run.py
@@ -69,6 +69,11 @@ def generate_and_send_midi(music_grammar, port_name, generation_length=3600, mem
             (p, a, d, w), memory = generate_grammar(music_grammar, memory, mem_length)
             # Generate midi message
             ########
+            # With a small probability, release the pedal
+            if random.random() < 0.05:
+                # Hold down the pedal (sometimes it releases if no message is sent)
+                msg = mido.Message('control_change', control=64, value=0)
+                port.send(msg)
             # Send midi message
             for i in range(len(p)):
                 if test:
@@ -82,13 +87,13 @@ def generate_and_send_midi(music_grammar, port_name, generation_length=3600, mem
 
                 # Wait for the next note
                 time.sleep(w[i] / 1000)
-            if random.random() < 0.1:
+            if random.random() < 0.2:
                 # Hold down the pedal (sometimes it releases if no message is sent)
                 msg = mido.Message('control_change', control=64, value=127)
                 port.send(msg)
             # Wait for the next generation
             ########
-            time.sleep(random.randint(1, 50) * 0.1)
+            time.sleep(random.randint(1, 50) * 0.2)
         except KeyboardInterrupt:
             if not test:
                 # Release the pedal
@@ -125,24 +130,35 @@ class GrammarGeneration:
 
     def define_grammar(self):
         return {
-            "Pattern": [[f"P{i}", f"A{i}", f"D{i}", f"W{i}"] for i in range(5)],
-            "P1": np.array([67, 75, 71, 78, 77, 88]),
+            "Pattern": [[f"P{i}", f"A{i}", f"D{i}", f"W{i}"] for i in range(7)],
+            "P0": np.array([60, 64, 67, 71, 74, 77, 80]),
+            "P1": np.array([72, 42, 73, 41, 76, 36, 75, 37]),
             "P2": np.array([27, 107, 62, 55, 71, 63]),
-            "P3": np.array([55, 62, 69, 76, 81, 82, 88]),
+            "P3": np.array([35 + i*7 for i in range(10)]),
             "P4": np.array([25, 108]),
-            "A1": np.array([30, 30, 30, 30, 30, 30]),
-            "A2": np.array([20, 20, 20, 20, 20, 20]),
-            "A3": np.array([20, 20, 20, 20, 20, 20, 20]),
-            "A4": np.array([20, 20]),
-            "D1": np.array([300, 300, 300, 300, 300, 300]),
+            "P5": np.array([27, 104, 60, 60, 60, 60]),
+            "P6": np.array([100 - i*5 for i in range(12)]),
+            "A0": np.array([random.randint(10, 45) for i in range(7)]),
+            "A1": np.array([25, 20, 25, 20, 35, 30, 25, 20]),
+            "A2": np.array([20, 30, 20, 20, 20, 20]),
+            "A3": np.array([random.randint(10, 40) for i in range(10)]),
+            "A4": np.array([20, 40]),
+            "A5": np.array([40, 40, 15, 15, 15, 15]),
+            "A6": np.array([random.randint(10, 40) for i in range(12)]),
+            "D0": np.array([2000 for i in range(7)]),
+            "D1": np.array([500, 500, 500, 500, 1000, 1000, 2000, 1000]),
             "D2": np.array([4000, 4000, 3000, 3000, 3000, 3000]),
-            "D3": np.array([3000, 3000, 3000, 3000, 3000, 3000, 3000]),
+            "D3": np.array([250 for i in range(10)]),
             "D4": np.array([3000, 3000]),
-            "W1": np.array([800, 800, 800, 800, 800, 800]),
+            "D5": np.array([4000, 4000, 100, 100, 100, 100]),
+            "D6": np.array([250 for i in range(12)]),
+            "W0": np.array([0 for i in range(6)] + [1000]),
+            "W1": np.array([0, 500, 0, 500, 0, 1000, 0, 2000]),
             "W2": np.array([0, 5000, 0, 0, 0, 3000]),
-            "W3": np.array([100, 100, 100, 100, 100, 100, 3000]),
-            "W4": np.array([0, 3000])
-
+            "W3": np.array([300 for i in range(9)] + [1000]),
+            "W4": np.array([0, 3000]),
+            "W5": np.array([0, 0, 250, 80, 300, 0]),
+            "W6": np.array([300 for i in range(11)] + [1000]),
         }
 
 
